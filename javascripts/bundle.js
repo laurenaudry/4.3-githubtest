406649f7b8d8583965a97007cb244c23c777134b
Lauren Johnson <laurenaudry1995@gmail.com>
2016-08-25 01:11:01 -0500

    html list made in javascript all forms linked

--- javascripts/bundle.js
+++ javascripts/bundle.js
@@ -1,5 +1,43 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-document.addEventListener("DOMContentLoaded", function(e){
+var gitform = this;
+
+gitform.getData = function(){
+  gitform.request = new XMLHttpRequest();
+
+  gitform.request.addEventListener("load", function(e) {
+    var responseJSON = JSON.parse(e.target.response);
+    var testElement = document.querySelector("[data-js='main']");
+
+    testElement.innerHTML = `
+    <img class="avatar_url" src="${gitform.getAvatar(responseJSON)}" alt="Lauren's profile picture" />
+    <h2>
+      ${gitform.getLogin(responseJSON)} 
+    </h2>
+    <p>
+      (${gitform.getUserType(responseJSON)})
+    </p>
+    <h4>
+      ${gitform.getLocation(responseJSON)}
+    </h4>
+    <h4>
+      <a href="mailto:${gitform.getEmail(responseJSON)}">${gitform.getEmail(responseJSON)}</a>
+    </h4>
+    <h4>
+      ${gitform.getHireable(responseJSON)}
+    </h4>
+
+    `;
+  });
+
+  gitform.request.open("GET", "https://api.github.com/users/laurenaudry");
+  gitform.request.send(null);
+}
+
+},{}],2:[function(require,module,exports){
+var gitform = require("./gitform.js");
+
+document.addEventListener("DOMContentLoaded", function(){
+  gitform.getData();
 });
 
-},{}]},{},[1]);
+},{"./gitform.js":1}]},{},[2]);
--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -1,7 +1,37 @@
 // Example spec
-// Import your files here... don't forget "../lib/<fileName>.js"
-describe("", function(){
-  it("", function(){
+var gitform = require("../lib/gitform.js");
 
+describe("gitform", function(){
+  var testing = {};
+
+  it("should get my profile picture from responseJSON", function(){
+    testing = {
+      avatar_url: "https://avatars.githubusercontent.com/u/20157258?v=3"
+    };
+    expect(gitHub.getAvatar(testing)).toEqual("hhttps://avatars.githubusercontent.com/u/20157258?v=3");
   });
-});
+
+  it("should get my login name from responseJSON", function(){
+    testing = {
+      login: "laurenaudry"
+    }
+    expect(gitHub.getLogin(testing)).toEqual("laurenaudry");
+  });
+
+  it("should show my account as a 'User'", function() {
+     stubJSON = {
+       type: "User"
+
+  it("should get my location from responseJSON", function() {
+    testing = {
+      location: "San Antonio, Tx"
+    };
+    expect(gitHub.getLocation(testing)).toEqual("San Antonio, Tx");
+  });
+
+  it("should get my email from responseJSON", function() {
+    testing = {
+      email: "laurenaudry1995@gmail.com"
+    };
+    expect(gitHub.getEmail(testing)).toEqual("laurenaudry1995@gmail.com");
+  });
